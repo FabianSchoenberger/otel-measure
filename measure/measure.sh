7591701c0113c63939220c4a5abdcab66967ecc2
FabianSchoenberger <163822878+FabianSchoenberger@users.noreply.github.com>
2025-06-19 12:00:33 +0200

    measure noop exporter

--- measure/measure.sh
+++ measure/measure.sh
@@ -18,13 +18,21 @@ runs=100
 #native_command="../manual/build/bin/linuxX64/releaseExecutable/manual.kexe"
 #native_file="manual/native.txt"
 
+# manual-noop
+jvm_command="java -jar ../manual-noop/build/lib/manual_noop-jvm-1.0.0.jar"
+jvm_file="manual-noop/jvm.txt"
+js_command="node ../manual-noop/build/js/packages/manual_noop/kotlin/manual_noop.js"
+js_file="manual-noop/js.txt"
+native_command="../manual-noop/build/bin/linuxX64/releaseExecutable/manual_noop.kexe"
+native_file="manual-noop/native.txt"
+
 # reference
-jvm_command="java -jar ../reference/build/lib/reference-jvm-1.0.0.jar"
-jvm_file="reference/jvm.txt"
-js_command="node ../reference/build/js/packages/reference/kotlin/reference.js"
-js_file="reference/js.txt"
-native_command="../reference/build/bin/linuxX64/releaseExecutable/reference.kexe"
-native_file="reference/native.txt"
+#jvm_command="java -jar ../reference/build/lib/reference-jvm-1.0.0.jar"
+#jvm_file="reference/jvm.txt"
+#js_command="node ../reference/build/js/packages/reference/kotlin/reference.js"
+#js_file="reference/js.txt"
+#native_command="../reference/build/bin/linuxX64/releaseExecutable/reference.kexe"
+#native_file="reference/native.txt"
 
 run() {
     command="$1"
